c32e7f41ee851736e6dda8f2d74f85a95db67ea2
Mentor <mentor@palokaj.co>
2021-10-28 16:26:40 +0200

    less naive loading manaer

--- src/components/view-qr.js
+++ src/components/view-qr.js
@@ -24,7 +24,7 @@ export default function ViewQR( ) {
   // Start code listener
   useEffect( f => listenToCode( newCode => {
     setCode( newCode.id )
-    setLoading( false )
+    if( newCode.id ) setLoading( false )
   } ), [] )
 
   // No code timeout
