f43a8474b24b4259d82651a54bfa04a07cdf3043
Mentor Palokaj <mentor@palokaj.co>
2022-06-08 15:08:49 +0200

    Revert "Revert "I18next integration""

---
+++ src/modules/i18n.js
@@ -0,0 +1,29 @@
+import i18n from 'i18next'
+import Backend from 'i18next-http-backend'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import { initReactI18next } from 'react-i18next'
+
+i18n
+	// load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
+	// learn more: https://github.com/i18next/i18next-http-backend
+	.use(Backend)
+
+	.use(LanguageDetector)
+
+	.use(initReactI18next)
+
+    .init({
+        backend: {
+            // translation file path
+            loadPath: '/locales/{{ns}}/{{lng}}.json',
+        },
+        supportedLngs: [ 'en', 'nl' ],
+        fallbackLng: 'en',
+        interpolation: {
+            escapeValue: false,
+        },
+        debug: process.env.NODE_ENV === 'development',
+        ns: [ 'dispenser' ], 
+    })
+
+export default i18n
