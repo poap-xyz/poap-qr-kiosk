69dd1ea62518a0bcee37352601df72702f263303
sami-reinoso <samanta@poap.io>
2023-01-10 14:48:20 -0300

    Fix: Code ariable name

--- src/components/organisms/EventCreate.js
+++ src/components/organisms/EventCreate.js
@@ -123,8 +123,8 @@ export default function Admin( ) {
         if( !cancelled ) setCodes( data )
 
         // Load event data based on codes
-        const code = data[0]?.trim()
-        const { data: { event, error } } = await getEventDataFromCode(code)
+        const formatedCode = data[0]?.trim()
+        const { data: { event, error } } = await getEventDataFromCode(formatedCode)
         log( 'Code data received ', event, error )
         if( error ) throw new Error( error )
         if( !event ) throw new Error( `${ t( 'create.event.eventExpired' ) }` )
