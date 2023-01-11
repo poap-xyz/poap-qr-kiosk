941267e9c8d4d08be94e133872d5344cb8ae410f
sami-reinoso <samanta@poap.io>
2023-01-11 10:50:10 -0300

    Fix: Resolves conflicts

--- src/components/organisms/EventCreate.js
+++ src/components/organisms/EventCreate.js
@@ -124,7 +124,8 @@ export default function Admin( ) {
                 if ( !cancelled ) setCodes( data )
 
                 // Load event data based on codes
-                const { data: { event, error } } = await getEventDataFromCode( data[0] )
+                const formatedCode = data[0]?.trim()
+                const { data: { event, error } } = await getEventDataFromCode( formatedCode )
                 log( 'Code data received ', event, error )
                 if ( error ) throw new Error( error )
                 if ( !event ) throw new Error( `${ t( 'create.event.eventExpired' ) }` )
