47ff09db87c2ba71b12e40c13e40654e7069441c
Ruben <ruben@volware.nl>
2023-08-01 16:00:10 +0200

    :wrench: ViewWrapper conditional header/main

--- src/components/molecules/Captcha.js
+++ src/components/molecules/Captcha.js
@@ -3,7 +3,7 @@ import ViewWrapper from '../molecules/ViewWrapper'
 import { Text } from '../atoms/Text'
 const { REACT_APP_recaptcha_v2_site_key } = process.env
 
-export default ( { ...props } ) => <ViewWrapper>
+export default ( { ...props } ) => <ViewWrapper center>
     <Text>Please check the box below to proceed.</Text>
     <ReCAPTCHA sitekey={ REACT_APP_recaptcha_v2_site_key } { ...props } />
 </ViewWrapper>
