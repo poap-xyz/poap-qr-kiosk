b802171a1714a895efcf9917af339043c06ad65a
Mentor <mentor@palokaj.co>
2023-09-05 17:53:06 +0200

    💚 Faulty env reference in tests

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -17,12 +17,12 @@
 import './commands'
 
 // Get the url of functions, to be used to call emulator or live url
-export const get_claim_function_url = env => {
+export const get_claim_function_url = () => {
 
     cy.log( `Generating link based on: `, Cypress.env() )
 
     const functions_emulator_port = 5001
-    const { REACT_APP_projectId, REACT_APP_useEmulator, REACT_APP_publicUrl } = env
+    const { REACT_APP_projectId, REACT_APP_useEmulator, REACT_APP_publicUrl } = Cypress.env()
 
     let url = ''
     if( REACT_APP_useEmulator !== 'true' ) url = `${ REACT_APP_publicUrl }/claim`
