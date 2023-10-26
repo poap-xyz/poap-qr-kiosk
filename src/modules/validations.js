0824b7e5f4c176b181191b42035e2ee04ec1d803
Mentor <mentor@palokaj.co>
2023-10-26 15:43:42 +0200

    🐛 fix regexes to allow for all TLDs

--- src/modules/validations.js
+++ src/modules/validations.js
@@ -1,2 +1,5 @@
-export const eth_address_or_ens_regex = /(0x[a-f0-9]{40})|(.*\.eth)/i
+// TLDs may be 1-63 characters long, see https://www.rfc-editor.org/rfc/rfc1034
+export const eth_address_or_ens_regex = /(0x[a-f0-9]{40})|(.*\.*{1-63})/i
+
+// Very naive regex validation for email addresses
 export const valid_email_regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
