f7e52297173d714ee560dd493af7a6e1fb8195f0
Mentor <mentor@palokaj.co>
2023-11-21 14:55:45 +0100

    🔼 dependencies passing tests

---
+++ .ncurc.js
@@ -0,0 +1,12 @@
+/* ///////////////////////////////
+// .ncurc configures how ncu behaves by default
+// docs: https://www.npmjs.com/package/npm-check-updates
+// /////////////////////////////*/
+module.exports = {
+    upgrade: false,
+    target: 'minor',
+    doctorTest: 'npm run test:ci:with_emulator',
+    reject: [
+        'cypress' // After cypress 11 a bunch of issues are introduced. It no longer can access non localhost:3000 urls (like emulators at localhost:8080) and doesn't allow for "cross-origin" redirects to localhost:5051
+    ]
+}
